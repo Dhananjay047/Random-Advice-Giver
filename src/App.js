b83bba73367f2bcd851f5d80704486c30e5691fa
Dhananjay047 <dhananjay.sahu.586@gmail.com>
2022-04-19 18:37:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState, useEffect } from 'react'
+import axios from "axios"
 
-function App() {
+import './App.css'
+
+
+const App = () => {
+
+    const [Advice, setAdvice ] =  useState("Advice")
+    
+
+    const fetchAdvice = async () => {
+      // var result = await axios.get('https://api.adviceslip.com/advice')
+      // setAdvice(result)
+      // console.log(result.data.slip.advice)
+    }
+
+    const getAdvice = () => {
+      axios.get('https://api.adviceslip.com/advice')
+        .then((response) => {
+          const { advice } =  response.data.slip
+          if(advice !== Advice ){
+            setAdvice(advice)
+          } else{
+            getAdvice()
+          }
+          //console.log(advice)
+        }).catch((err)=>{
+          console.log(err)
+        })
+    }
+
+    useEffect(() => {
+      getAdvice()
+    },[])
+    
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <div className="card">
+          <h1 className="heading">{Advice}</h1>
+          <button className="button" onClick={getAdvice}>
+            <span>GIVE ME ADVICE!</span>
+          </button>
+        </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
